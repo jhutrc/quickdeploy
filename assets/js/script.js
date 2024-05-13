352a9e059cc734e36a6649c3c0b9ad911a1717a3
jhustata <127160147+jhustata@users.noreply.github.com>
2024-05-13 19:22:49 -0400

    betas fixed. next s0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 'use strict';
 
-var scenarioVector = [1, 0, 0]; // Default to living donors scenario
+var scenarioVector = [1, 0, 0, 0, 0]; // Default to excellent self-rated health scenario
 
 function selectScenario(scenario) {
   switch (scenario) {
-    case 'livingDonors':
-      scenarioVector = [1, 0, 0];
+    case 'excellent':
+      scenarioVector = [1, 0, 0, 0, 0];
       break;
-    case 'healthyNonDonors':
-      scenarioVector = [0, 1, 0];
+    case 'verygood':
+      scenarioVector = [0, 1, 0, 0, 0];
       break;
-    case 'generalPopulation':
-      scenarioVector = [0, 0, 1];
+    case 'good':
+      scenarioVector = [0, 0, 1, 0, 0];
+      break;
+    case 'fair':
+      scenarioVector = [0, 0, 0, 1, 0];
+      break;
+    case 'poor':
+      scenarioVector = [0, 0, 0, 0, 1];
       break;
     default:
-      scenarioVector = [1, 0, 0];
+      scenarioVector = [1, 0, 0, 0, 0];
   }
 }
 
 function calculateMortalityRisk() {
-  const beta = [0, 1.468284, 3.291419]; // Beta coefficients for living donors, healthy non-donors, general population
+  const beta = [0, .29266961, .63127178, 1.0919233, 2.010895]; // Beta coefficients for excellent, very good, good, fair, poor
   const s0 = [.9999999, .9949034, .9844462, .9792454]; // Survival probabilities at timepoints 0, 5, 12, 15
   const timePoints = [0, 5, 12, 15];
   const logHR = beta.reduce((acc, curr, index) => acc + (curr * scenarioVector[index]), 0);
